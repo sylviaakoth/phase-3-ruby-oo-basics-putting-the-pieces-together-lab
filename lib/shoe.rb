c7423b4c60d78467224c9a4436db1f8315f38e01
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-10-24 12:55:54 +0300

    commit tree 732e907b9c5e66c1f114fc29949aa1fba9d02ed9

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize (brand)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
